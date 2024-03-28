1bb0a963e400b1a07da7f3b7953a26811d946ee3
Tommy Gaessler <tommy.gaessler@gmail.com>
2024-03-28 11:47:25 -0600

    updated to 3.5.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, NgZone } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DOCUMENT } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
@@ -12,34 +12,27 @@ import ZoomMtgEmbedded from '@zoom/meetingsdk/embedded';
 })
 export class AppComponent implements OnInit {
 
-  // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
   signatureEndpoint = 'https://26040c9x85.execute-api.us-west-1.amazonaws.com/default/meetingsdk'
-  sdkKey = ''
+  sdkKey = 'viodIoBwGbXY8HfsCADKrAIqOgdLuwLNPVUI'
   meetingNumber = ''
   role = 0
   userName = 'Zoom Meeting SDK'
   userEmail = ''
   passWord = ''
-  // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-  // Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-registered
-  // Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-registered
   registrantToken = ''
   zakToken = ''
 
   client = ZoomMtgEmbedded.createClient();
 
-  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document, private activatedRoute: ActivatedRoute) {
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document, private activatedRoute: ActivatedRoute, private ngZone: NgZone) {
     this.activatedRoute.queryParams.subscribe(params => {
       this.meetingNumber = params['meeting'];
       this.passWord = params['passcode'];
-      this.sdkKey = params['key'];
     });
   }
 
   ngOnInit() {
-    let meetingSDKElement = document.getElementById('meetingSDKElement');
 
-    this.client.init({ zoomAppRoot: meetingSDKElement, language: 'en-US', patchJsMedia: true });
   }
 
   getSignature() {
@@ -59,15 +52,27 @@ export class AppComponent implements OnInit {
 
   startMeeting(signature) {
 
-    this.client.join({
-    	sdkKey: this.sdkKey,
-    	signature: signature,
-    	meetingNumber: this.meetingNumber,
-    	password: this.passWord,
-    	userName: this.userName,
-      userEmail: this.userEmail,
-      tk: this.registrantToken,
-      zak: this.zakToken
+    let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+    this.ngZone.runOutsideAngular(() => {
+      this.client.init({ zoomAppRoot: meetingSDKElement, language: 'en-US', patchJsMedia: true }).then(() => {
+        this.client.join({
+          sdkKey: this.sdkKey,
+          signature: signature,
+          meetingNumber: this.meetingNumber,
+          password: this.passWord,
+          userName: this.userName,
+          userEmail: this.userEmail,
+          tk: this.registrantToken,
+          zak: this.zakToken
+        }).then(() => {
+          console.log('joined successfully')
+        }).catch((error) => {
+          console.log(error)
+        })
+      }).catch((error) => {
+        console.log(error)
+      })
     })
   }
 }
